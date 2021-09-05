7f0e1e3f732b0e73f5430571d59ee77361da0099
Muky-dev <ti.gabrielfini@gmail.com>
2021-09-05 12:19:02 -0300

    First chat POC

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,30 @@
 import express, { Express } from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
 import mongoose from 'mongoose'
 
 const app: Express = express()
+const httpServer = createServer(app)
 
 const port: string | number | undefined = process.env.PORT || 4000
 const MONGO_URI: string | undefined = process.env.DB_URI || 'Error'
+
+const io = new Server(httpServer)
+io.on('connection', (socket: Socket) => {
+    console.log(`${socket.id} connected `)
+    socket.on('message', (evt) => {
+        socket.broadcast.emit('message', evt)
+    })
+})
+
+/* ROUTES BELOW HERE */
 
 mongoose.connect(MONGO_URI)
 
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
-db.once('connect', () => {
+db.once('open', () => {
     console.log('Database connected')
 })
 
-app.listen(port, () => {
-    console.log(`Server running on: ${port}`)
-})
+httpServer.listen(port)
