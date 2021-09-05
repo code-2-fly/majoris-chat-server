3c1341e00bc374fcecfeabfa19889ed07cb5b47a
Muky-dev <ti.gabrielfini@gmail.com>
2021-09-05 16:43:12 -0300

    Routes after mongo connection

--- src/app.ts
+++ src/app.ts
@@ -9,22 +9,21 @@ const httpServer = createServer(app)
 const port: string | number | undefined = process.env.PORT || 4000
 const MONGO_URI: string | undefined = process.env.DB_URI || 'Error'
 
-const io = new Server(httpServer)
-io.on('connection', (socket: Socket) => {
-    console.log(`${socket.id} connected `)
-    socket.on('message', (evt) => {
-        socket.broadcast.emit('message', evt)
-    })
-})
-
-/* ROUTES BELOW HERE */
-
 mongoose.connect(MONGO_URI)
 
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => {
     console.log('Database connected')
+})
+
+/* ROUTES BELOW HERE */
+const io = new Server(httpServer)
+io.on('connection', (socket: Socket) => {
+    console.log(`${socket.id} connected `)
+    socket.on('message', (evt) => {
+        socket.broadcast.emit('message', evt)
+    })
 })
 
 httpServer.listen(port)
