65ba650fec5b5c39c9eceb85e12d03a4acdde16d
rootslowed <rootslowed@gmail.com>
2021-09-07 22:49:23 -0300

    app listen method removed

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,28 @@
 import express from "express";
-import * as http from "http";
-import * as socketIo from "socket.io";
+import http from "http";
+import socketIo from "socket.io";
 
-class App {
+class app {
 
-  public PORT: number = 4000;
+  public readonly PORT: number = 4000;
   public app!: express.Application;
   public server!: http.Server;
-  private io!: socketIo.Server;
+  public io!: socketIo.Server;
 
   constructor() {
-
     this.routes();
     this.sockets();
-    this.listen();
-
   }
 
-  public routes() {
-
+  private routes() {
     this.app = express();
-
   }
 
   private sockets(): void {
-
     this.server = http.createServer(this.app);
-
     this.io = new socketIo.Server(this.server);
-
-  }
-
-  private listen(): void {
-
-    this.io.on("connection", (socket: socketIo.Socket) => {
-      console.log(`a user connected: ${socket.id}`);
-
-      socket.on("message", function (data: any) {
-        console.log(data.cmd);
-        socket.broadcast.emit('message', data);
-      });
-
-      socket.on("disconnect", () => {
-        console.log("user disconnected");
-      });
-
-    });
-
   }
 
 }
 
-export default new App;
+export default new app;
