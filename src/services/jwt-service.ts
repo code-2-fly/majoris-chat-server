63f19fe3934bc95515fda38a417f855c78295f53
Muky-dev <ti.gabrielfini@gmail.com>
2021-09-09 17:34:38 -0300

    JWT service implemented

---
+++ src/services/jwt-service.ts
@@ -0,0 +1,31 @@
+import { sign, verify } from "jsonwebtoken"
+
+class jwtService {
+  private static secret = process.env.JWT_KEY || ""
+
+  public static createAccess = async (payload: any, expiresIn: string) => {
+    return new Promise((resolve, reject) => {
+      sign(payload, this.secret, { expiresIn: expiresIn }, (err, token) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(token)
+        }
+      })
+    })
+  }
+
+  public static verify = async (payload: any) => {
+    return new Promise((resolve, reject) => {
+      verify(payload, this.secret, (err: Error | null, decoded: any) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(decoded)
+        }
+      })
+    })
+  }
+}
+
+export default jwtService
