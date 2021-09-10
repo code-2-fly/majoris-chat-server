a21e2c437f0a294fac219ebf82808de9c955e5e1
Muky-dev <ti.gabrielfini@gmail.com>
2021-09-10 12:57:02 -0300

    Promise returns any value

--- src/services/jwt-service.ts
+++ src/services/jwt-service.ts
@@ -3,7 +3,10 @@ import { sign, verify } from "jsonwebtoken"
 class jwtService {
   private static secret = process.env.JWT_KEY || ""
 
-  public static createAccess = async (payload: any, expiresIn: string) => {
+  public static createAccess = async (
+    payload: any,
+    expiresIn: string,
+  ): Promise<any> => {
     return new Promise((resolve, reject) => {
       sign(payload, this.secret, { expiresIn: expiresIn }, (err, token) => {
         if (err) {
@@ -15,7 +18,7 @@ class jwtService {
     })
   }
 
-  public static verify = async (payload: any) => {
+  public static verify = async (payload: any): Promise<any> => {
     return new Promise((resolve, reject) => {
       verify(payload, this.secret, (err: Error | null, decoded: any) => {
         if (err) {
